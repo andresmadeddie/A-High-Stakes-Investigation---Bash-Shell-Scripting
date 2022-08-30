28a6fdaa815d0d29d64949d122ba4885bbcb7112
andresmadeddie <abinary1010@gmail.com>
2022-08-29 22:22:10 -0400

    Upload portada, update scripts

--- Dealer Finder interactive script.md
+++ Dealer Finder interactive script.md
@@ -6,6 +6,6 @@ Script: [Dealer Finder by Time and Game](/Scripts/dealer_%20finder_by_time_and_g
 
 After the impressive results of the above investigation, you are tasked with creating a shell script that can easily analyze future employee schedules. They will use this to determine which employee was working at a specific time in case of future losses.
 
-The script prompts the user to enter a date, time, and game. Then, it displays the name of the dealer that matches those parameters. The following is a screenshot of the executed script.
+The script prompts the user to enter a date, time, and game. Then, it displays the time and name of the dealer that matches those parameters. The following is a screenshot of the executed script.
 
 ![Dealer Finder Script Screen Shot](/Images/output2.PNG)
--- Scripts/dealer_ finder_by_time_and_game.sh
+++ Scripts/dealer_ finder_by_time_and_game.sh
@@ -2,32 +2,53 @@
 
 # Users inputs
 read -p "input date(MMDD): " date
-read -p "input hour (HH): " h
-read -p 'input AM or PM:' m
-read -p "enter letter game(b for BlackJack / r for Roulette / t for TexasHoldE): " g
-
-date=0311
-h=01
-m=PM
-g=r
+read -p "input hour (HH): " hour
+read -p 'input AM or PM:' meridian
+read -p "enter letter game(b for BlackJack / r for Roulette / t for TexasHoldE): " game
 
 # Variables
 path=~/Lucky_Duck_Investigations/Dealer_Schedules_0310/
-time=$(echo "$h:00:00 $m")
+time=$(echo "$hour:00:00 $meridian")
 
+
+# FORMAT VALIDATION
+# Validate date format
+if [[ ! $date =~ ^[0-9]{4}$ ]]; then
+    echo -e "Wrong date input. Please input date format (MMDD) Ex: 0311"
+    exit 1
+# Validate time format
+elif [[ ! $hour =~ ^0[1-9]|1[0-2]$ ]]; then
+    echo -e "Wrong hour input. Please input an hour between 01 and 12\nFormat: HH\nExample: 01"
+    exit 1
+# Validate Meridian format
+elif [[ ! $meridian =~ ^[PApa][Mm]$ ]]; then
+    echo -e "Wrong meridian input. Please input AM or PM"
+    exit 1
+fi
+
+
+# EXISTENCE VALIDATION
 # Validate date exists in the database
 if [ ! -f "$path$date"_Dealer_schedule ]; then
-    echo 'The date does not match any date on the database. Date input example for March eleven: 0311'
+    echo -e "The date does not match any date on the database/n Current available dates are:\n$(ls $path | sed 's/_Dealer_schedule//')"
     exit 1
-#elif [ ! $(grep -q "$time" "$path$date"_Dealer_schedule) eq 1 ]; then
-#elif [ $(ls $path | grep -q "$date"_Dealer_schedule) eq 1 ]; then
-    echo -e 'The hour does not match any hour on the database./n hour input example for 1:00 PM: 01/n AM/PM example: PM'
-elif [ $g == 'b' ]; then
-    cat "$path$date"_Dealer_schedule | awk '{print $1, $2, $3, $4}' | grep "$time"
-elif [ $g == 'r' ]; then
-    cat "$path$date"_Dealer_schedule | awk '{print $1, $2, $5, $6}' | grep "$time"
-elif [ $g == 't' ]; then 
-    cat "$path$date"_Dealer_schedule | awk '{print $1, $2, $7, $8}' | grep "$time"
-else
-    echo wrong game input
+# Validate hour exists in th database
+elif [ $(ls $path | grep -q "$date") ]; then
+    echo -e "The hour does not match any hour on the database.\n Current available times for $date are:\n$(cat "$path$date"_Dealer_schedule | awk '{print $1, $2}')"
+    exit 1
+# Validate game input
+elif [[ ! $game =~ ^[btr]$ ]]; then
+    echo -e "Wrong game input. Please choose between b, r, or t"
+    exit 1
 fi
+
+# Display the query
+echo -e "\n\n TIME       DEALER'S NAME\n"
+if [ $game = 'b' ]; then
+    cat "$path$date"_Dealer_schedule | awk '{print $1, $2, $3, $4}' | grep "$time"
+elif [ $game = 'r' ]; then
+    cat "$path$date"_Dealer_schedule | awk '{print $1, $2, $5, $6}' | grep "$time"
+elif [ $game = 't' ]; then
+    cat "$path$date"_Dealer_schedule | awk '{print $1, $2, $7, $8}' | grep "$time"
+fi
+echo -e "\n\n"
